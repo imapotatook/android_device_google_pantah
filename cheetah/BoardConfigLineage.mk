3e515a3e96c53290f88218d1e433bab8d614785f
imapotatook <imapotatook123@gmail.com>
2023-04-23 02:47:31 +0930

    remove left over lineage files

--- cheetah/BoardConfigLineage.mk
+++
@@ -1,9 +0,0 @@
-#
-# Copyright (C) 2022 The LineageOS Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-include device/google/pantah/BoardConfigLineage.mk
-
-include vendor/google/cheetah/BoardConfigVendor.mk
--- panther/BoardConfigLineage.mk
+++
@@ -1,9 +0,0 @@
-#
-# Copyright (C) 2022 The LineageOS Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-include device/google/pantah/BoardConfigVoltage.mk
-
-include vendor/google/panther/BoardConfigVendor.mk
